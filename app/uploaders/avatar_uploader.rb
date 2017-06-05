9335053be987959f76ffb6b5e2f2d4e15ee6f7dc
cl <cl_fighting@163.com>
2017-06-05 16:30:03 +0800

    redefine pic size

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -53,7 +53,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   process resize_to_fill: [800,800]                      # 默认上传的图片被减成800*800
 
   version :medium do                                     # 自己重新定义图片大小
-    process resize_to_fill: [600,300]
+    process resize_to_fill: [600,400]
   end
   version :thumb do
     process resize_to_fill: [200,200]
