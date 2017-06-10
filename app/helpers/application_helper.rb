01d6d63c55811c24456bc8bf095f58d77d478a34
cl <cl_fighting@163.com>
2017-06-10 12:15:51 +0800

    change distance_of_time_in_words

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,23 +29,23 @@ module ApplicationHelper
           when 0..1
               return (distance_in_minutes == 0) ? '不到 1 分钟' : '1 分钟' unless include_seconds
           case distance_in_seconds
-              when 0..4   then '不到 5 秒'
-              when 5..9   then '不到 10 秒'
-              when 10..19 then '不到 20 秒'
+              when 0..4   then '不到5秒'
+              when 5..9   then '不到10秒'
+              when 10..19 then '不到20秒'
               when 20..39 then '半分钟'
-              when 40..59 then '不到 1 分钟'
-              else             '1 分钟'
+              when 40..59 then '不到1分钟'
+              else             '1分钟'
           end
 
-          when 2..44           then "#{distance_in_minutes} 分钟"
-          when 45..89          then '约 1 小时'
-          when 90..1439        then "约 #{(distance_in_minutes.to_f / 60.0).round} 小时"
-          when 1440..2879      then '1 天'
-          when 2880..43199     then "#{(distance_in_minutes / 1440).round} 天"
-          when 43200..86399    then '约 1 个月'
-          when 86400..525599   then "#{(distance_in_minutes / 43200).round} 个月"
-          when 525600..1051199 then '约 1 年'
-         else                      "#{(distance_in_minutes / 525600).round} 年"
+          when 2..44           then "#{distance_in_minutes}分钟"
+          when 45..89          then '约1小时'
+          when 90..1439        then "约 #{(distance_in_minutes.to_f / 60.0).round}小时"
+          when 1440..2879      then '1天'
+          when 2880..43199     then "#{(distance_in_minutes / 1440).round}天"
+          when 43200..86399    then '约1个月'
+          when 86400..525599   then "#{(distance_in_minutes / 43200).round}个月"
+          when 525600..1051199 then '约1年'
+         else                      "#{(distance_in_minutes / 525600).round}年"
      end
   end
 
