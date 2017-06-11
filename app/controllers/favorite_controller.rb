6fbf52701c8f135f2bbeb8ecbbaeac333b30c9be
cl <cl_fighting@163.com>
2017-06-11 13:20:07 +0800

    beauty favorate web

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -1,7 +1,7 @@
 class FavoriteController < ApplicationController
 
     def index
-      @products = current_user.favorite_products
+      @products = current_user.favorite_products.paginate(:page => params[:page], :per_page => 9)
     end
 
 end
